aca1619c96c993160fa97971e04bd0965bccc93c
Chris Hunt <c@chrishunt.co>
2013-04-30 07:03:47 -0700

    Initial commit

---
+++ lib/cane/hashcheck.rb
@@ -0,0 +1,7 @@
+require "cane/hashcheck/version"
+
+module Cane
+  module Hashcheck
+    # Your code goes here...
+  end
+end
---
+++ lib/cane/hashcheck/version.rb
@@ -0,0 +1,5 @@
+module Cane
+  module Hashcheck
+    VERSION = "0.0.1"
+  end
+end
